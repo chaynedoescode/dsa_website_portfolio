cc302085f96d42f494dbb86702e6662723c84010
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2025-01-19 20:43:10 +0800

    to dolist

---
+++ todolist.py
@@ -0,0 +1,18 @@
+# To do:
+# About us:
+# - clickable + more info 
+# LinkedList:
+# - palakihin 
+# - auto print
+# stack:
+# - palakihin
+# Queue:
+# - Merge queue and deque
+# - auto print both
+# - output box individual 
+# Binary Tree:
+# -
+# - 
+# - 
+# Graph:
+# - Add Graph
