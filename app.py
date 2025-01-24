54247859d878ae28f1e7fad4987ea5818944abcd
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2025-01-24 21:23:40 +0800

    will automatically print linked list without clicking the print button

--- app.py
+++ app.py
@@ -22,43 +22,33 @@ def linked_list_view():
 
         if action == 'insert_beginning':
             linked_list.insert_at_beginning(user_input)
-            success_message = "Item successfully inserted at the beginning!"
 
         elif action == 'insert_end':
             linked_list.insert_at_end(user_input)
-            success_message = "Item successfully inserted at the End!"
 
         elif action == 'remove_beginning':
-            if linked_list.remove_beginning():
-                success_message = "Successfully Removed from Beginning!"
-            else:
-                success_message = "Failed to Remove from Beginning. The list is empty."
+            linked_list.remove_beginning()
 
         elif action == 'remove_end':
-            if linked_list.remove_at_end():
-                success_message = "Successfully Removed from End!"
-            else:
-                success_message = "Failed to Remove from End. The list is empty."
+            linked_list.remove_at_end()
 
         elif action == 'remove_position':
-            if linked_list.remove_at(user_input):
-                success_message = f"Successfully Removed {user_input}!"
-            else:
-                success_message = f"Failed to Remove {user_input}. The item was not found."
+            linked_list.remove_at(user_input)
 
         elif action == 'search':
             if linked_list.search(user_input):
                 success_message = "Item Found"
             else:
                 success_message = "Item Not Found"
+            return render_template('LINKED LIST.html', success_message=success_message)
 
-        elif action == 'print':
-            result = linked_list.print_linked_list()
-            if result == False:
-                success_message = "Linked list is Empty"
-            else:
-                success_message = f"{result}"       
-            
+        # Automatically print the linked list after each operation
+        result = linked_list.print_linked_list()
+        if result == False:
+            success_message = "Linked list is Empty"
+        else:
+            success_message = result
+
     return render_template('LINKED LIST.html', success_message=success_message)
 
 @app.route('/stack', methods=["POST", "GET"])
